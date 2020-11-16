52a6edfcd6698947cfb630c9139231bd414bfa1e
Amir Rastar <amir.rastar@gmail.com>
2020-11-16 13:05:23 +1000

    first

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
+        'Access-Control-Max-Age': '1800',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
